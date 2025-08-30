0693c97efc67da1dcd3aa5c1f928c08316f08d01
Douglas Marques Alves <douglas.marq.alves@outlook.com>
2025-08-30 15:10:34 -0300

    chore: adding validation

--- handlers/deploy.go
+++ handlers/deploy.go
@@ -14,9 +14,26 @@ var (
 )
 
 func DeployHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		response.Message = "method not allowed"
+		api.WriteToJSON(w,
+			response,
+			http.StatusMethodNotAllowed)
+		return
+	}
+
 	var payload constants.DeploymentRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		response.Message = "invalid body"
+		api.WriteToJSON(w,
+			response,
+			http.StatusBadRequest)
+		return
+	}
+
+	if (payload.ScriptType == "" || len(payload.ScriptType) == 0) ||
+		(payload.FullPath == "" || len(payload.FullPath) == 0) {
 		response.Message = "invalid body"
 		api.WriteToJSON(w,
 			response,
